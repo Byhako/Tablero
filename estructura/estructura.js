8ebcb5682d510e7406cd58d61103da2d0010de6e
Ruben E. Acosta <eldragondera1@gmail.com>
2018-07-26 13:51:33 -0500

    actualizndo grafico estructura

--- estructura/estructura.js
+++ estructura/estructura.js
@@ -1,34 +1,38 @@
-// { "from": 0, "to": 0, "text": "up or timer", "curviness": -20 }
+var data = {}
+
+$.get('./data.json', function(file) {
+  console.log(file)
+}, 'json')
 
 function load(data) {
   data = { "nodeKeyProperty": "id",
-    "nodeDataArray": [
-      { "id": 0, "loc": "0 0", "text": "APP", color: "red"},
-      { "id": 1, "loc": "-100 0", "text": "Base", color: "#AD7FA8FF" },
-      { "id": 2, "loc": "100 0", "text": "Board", color: "#AD7FA8FF" },
-      { "id": 3, "loc": "150 -100", "text": "CardsContainer", color: "#204A87FF" },
-      { "id": 4, "loc": "150 100", "text": "CustomDragLayer", color: "#4E9A06FF" },
-      { "id": 5, "loc": "300 160", "text": "CardDragPreview", color: "#73D216FF"},
-      { "id": 6, "loc": "300 40", "text": "snapToGrid", color: "#73D216FF" },
-      { "id": 7, "loc": "350 -100", "text": "Cards", color: "#3465A4FF"},
-      { "id": 8, "loc": "480 -100", "text": "DraggableCard", color: "#3465A4FF" },
-      { "id": 9, "loc": "650 -100", "text": "Card", color: "#3465A4FF" },
-      { "id": 10, "loc": "0 100", "text": "actions/lists" }
+  "nodeDataArray": [
+    {"id":"0", "loc":"-67 -7", "text":"Router", "color":"red"},
+    {"id":"2", "loc":"83 -2", "text":"Board", "color":"#AD7FA8FF"},
+    {"id":"3", "loc":"150 -100", "text":"CardsContainer", "color":"#204A87FF"},
+    {"id":"4", "loc":"150 100", "text":"CustomDragLayer", "color":"#4E9A06FF"},
+    {"id":"5", "loc":"413 24", "text":"CardDragPreview", "color":"#73D216FF"},
+    {"id":"6", "loc":"424 129", "text":"snapToGrid", "color":"#73D216FF"},
+    {"id":"7", "loc":"350 -100", "text":"Cards", "color":"#3465A4FF"},
+    {"id":"8", "loc":"480 -100", "text":"DraggableCard", "color":"#3465A4FF"},
+    {"id":"9", "loc":"650 -100", "text":"Card", "color":"#3465A4FF"},
+    {"id":"-10", "loc":"-210 -22", "text":"index", "color":"red"},
+    {"id":"-11", "loc":"284 -175", "text":"Vista de tarjetas y columnas estáticas", "color":"white"},
+    {"id":"-12", "loc":"191 183", "text":"Vista cuando se mueve cada tarjeta", "color":"white"},
     ],
     "linkDataArray": [
-      { "from": 0, "to": 1, color: "#EF2929FF"},
-      { "from": 0, "to": 2, color: "#EF2929FF"},
-      { "from": 2, "to": 3, color: "blue"},
-      { "from": 2, "to": 4, color: "green"},
-      { "from": 4, "to": 5, color: "green"},
-      { "from": 4, "to": 6, color: "green"},
-      { "from": 3, "to": 7, color: "blue"},
-      { "from": 7, "to": 8, color: "blue"},
-      { "from": 8, "to": 9, color: "blue"},
-      { "from": 5, "to": 9, color: "green" },
-      { "from": 2, "to": 10 }
+    {"from":"0", "to":"2", "color":"#EF2929"},
+    {"from":"2", "to":"3", "color":"blue"},
+    {"from":"2", "to":"4", "color":"green"},
+    {"from":"4", "to":"6", "color":"green"},
+    {"from":"3", "to":"7", "color":"blue"},
+    {"from":"7", "to":"8", "color":"blue"},
+    {"from":"8", "to":"9", "color":"blue"},
+    {"from":"5", "to":"9", "color":"green"},
+    {"from":"-10", "to":"0", "color":"#EF2929"},
+    {"from":"4", "to":"5", "color":"green"},
     ]
-  }
+}
   myDiagram.model = go.Model.fromJson(data);
 }
 
@@ -48,7 +52,8 @@ function init() {
       });
   // when the document is modified, add a "*" to the title and enable the "Save" button
   myDiagram.addDiagramListener("Modified", function(e) {
-    
+    var button = document.getElementById("SaveButton");
+    if (button) button.disabled = !myDiagram.isModified;
     var idx = document.title.indexOf("*");
     if (myDiagram.isModified) {
       if (idx < 0) document.title += "*";
@@ -161,186 +166,47 @@ function init() {
       )
     );
   // read in the JSON data from the "mySavedModel" element
-  load();
+  load(data);
+}
+
+function save() {
+  const model = JSON.parse(myDiagram.model.toJson())
+  var myModel = '{"nodeDataArray": [\n'
+  model.nodeDataArray.map((nodo) => {
+    const id = nodo.id.toString()
+
+    // obtengo solo parte entera de loc
+    var loc = nodo.loc.toString()
+    loc = loc.split(" ")
+    const n1 = parseInt(loc[0]).toString()
+    const n2 = parseInt(loc[1]).toString()
+    loc = n1 + ' ' + n2
+    const text = nodo.text.toString()
+    var color = "yellow"
+    try {
+      color = nodo.color.toString()
+    }
+    catch(err) {
+    }
+    myModel = myModel + '{"id":"' + id + '", "loc":"' + loc + '", "text":"' + 
+    text + '", "color":"' + color + '"},\n'
+  })
+
+  myModel = myModel + '],\n"linkDataArray": [\n'
+  model.linkDataArray.map((link) => {
+    let fo = link.from.toString()
+    let to = link.to.toString()
+    var color = "black"
+    try {
+      color = link.color.toString()
+    }
+    catch(err) {
+    }
+    
+    myModel = myModel + '{"from":"' + fo + '", "to":"' + to + '", "color":"' + color + '"},\n'
+  })
+  myModel = myModel + ']\n}'
+  document.getElementById("mySavedModel").value = myModel
 }
 
 
-/*
-
-
-function init() {
-  var $ = go.GraphObject.make;
-  myDiagram =
-    $(go.Diagram, "myDiagramDiv", // the ID of the DIV HTML element
-      {
-        initialContentAlignment: go.Spot.Center,
-        "Changed": invalidateLinkRoutes,
-        "undoManager.isEnabled": true
-      });
-  myDiagram.nodeTemplate =
-    $(go.Node, go.Panel.Auto,
-      { locationSpot: go.Spot.Center },
-      new go.Binding("location", "loc", go.Point.parse),
-      $(go.Shape,
-        { figure: "Procedure", fill: "white" },
-        new go.Binding("fill", "color")),
-      $(go.TextBlock,
-        { font: "bold 11pt sans-serif" },
-        new go.Binding("text"))
-    );
-  myDiagram.linkTemplate =
-    $(MultiNodePathLink,  // subclass of Link, defined below
-      go.Link.Bezier,
-      { layerName: "Background", toShortLength: 4 },
-      $(go.Shape, { strokeWidth: 4 },
-        new go.Binding("stroke", "color")),
-      $(go.Shape, { toArrow: "Standard", scale: 3, strokeWidth: 0 },
-        new go.Binding("fill", "color"))
-    );
-  function invalidateLinkRoutes(e) {
-    // when a Node is moved, invalidate the route for all MultiNodePathLinks that go through it
-    if (e.change === go.ChangedEvent.Property && e.propertyName === "location" && e.object instanceof go.Node) {
-      var diagram = e.diagram;
-      var node = e.object;
-      if (node._PathLinks) {
-        node._PathLinks.each(function(l) { l.invalidateRoute(); });
-      }
-    } else if (e.change === go.ChangedEvent.Remove && e.object instanceof go.Layer) {
-      // when a Node is deleted that has MultiNodePathLinks going through it, invalidate those link routes
-      if (e.oldValue instanceof go.Node) {
-        var node = e.oldValue;
-        if (node._PathLinks) {
-          node._PathLinks.each(function(l) { l.invalidateRoute(); });
-        }
-      } else if (e.oldValue instanceof MultiNodePathLink) {
-        // when deleting a MultiNodePathLink, remove all references to it in Node._PathLinks
-        var link = e.oldValue;
-        var diagram = e.diagram;
-        var midkeys = link.data.path;
-        if (Array.isArray(midkeys)) {
-          for (var i = 0; i < midkeys.length; i++) {
-            var node = diagram.findNodeForKey(midkeys[i]);
-            if (node !== null && node._PathLinks) node._PathLinks.remove(link);
-          }
-        }
-      }
-    }
-  }
-  // create a few nodes and links
-  myDiagram.model = new go.GraphLinksModel([
-      { key: 1, text: "Alpha mi mama me mimi", color: "lightyellow", loc: "0 0" },
-      { key: 2, text: " Beta ", color: "brown", loc: "200 0" },
-      { key: 3, text: "Gamma", color: "green", loc: "300 100" },
-      { key: 4, text: "Delta", color: "slateblue", loc: "100 200" },
-      { key: 5, text: "Epsilon", color: "aquamarine", loc: "300 350" },
-      { key: 6, text: "Zeta", color: "tomato", loc: "0 100" },
-      { key: 7, text: "Eta", color: "goldenrod", loc: "0 300" },
-      { key: 8, text: "Theta", color: "orange", loc: "300 200" },
-  ], [
-      { from: 1, to: 5, path: [2, 3, 4], color: "blue" },
-      { from: 6, to: 5, path: [7, 4, 8], color: "red" }
-  ]);
-}
-function MultiNodePathLink() {
-  go.Link.call(this);
-}
-go.Diagram.inherit(MultiNodePathLink, go.Link);
-// ignores this.routing, this.adjusting, this.corner, this.smoothness, this.curviness
-
-MultiNodePathLink.prototype.computePoints = function() {
-  // get the list of Nodes that should be along the path
-  var nodes = [];
-  if (this.fromNode !== null && this.fromNode.location.isReal()) {
-    nodes.push(this.fromNode);
-  }
-  var midkeys = this.data.path;
-  if (Array.isArray(midkeys)) {
-    var diagram = this.diagram;
-    for (var i = 0; i < midkeys.length; i++) {
-      var node = diagram.findNodeForKey(midkeys[i]);
-      if (node instanceof go.Node && node.location.isReal()) {
-        nodes.push(node);
-        // Optimization?: remember on each path Node all of
-        // the MultiNodePathLinks that go through it;
-        // but this optimization requires maintaining this cache
-        // in a Diagram Changed event listener.
-        var set = node._PathLinks;
-        if (!set) set = node._PathLinks = new go.Set(go.Link);
-        set.add(this);
-      }
-    }
-  }
-  if (this.toNode !== null && this.toNode.location.isReal()) {
-    nodes.push(this.toNode);
-  }
-  // now do the routing
-  this.clearPoints();
-  var prevloc = null;
-  var thisloc = null;
-  var nextloc = null;
-  for (var i = 0; i < nodes.length; i++) {
-    var node = nodes[i];
-    thisloc = node.location;
-    nextloc = (i < nodes.length - 1) ? nodes[i + 1].location : null;
-    var prevpt = null;
-    var nextpt = null;
-    if (this.curve === go.Link.Bezier) {
-      if (prevloc !== null && nextloc !== null) {
-        var prevang = thisloc.directionPoint(prevloc);
-        var nextang = thisloc.directionPoint(nextloc);
-        var avg = (prevang + nextang) / 2;
-        var clockwise = prevang > nextang;
-        if (Math.abs(prevang - nextang) > 180) {
-          avg += 180;
-          clockwise = !clockwise;
-        }
-        if (avg >= 360) avg -= 360;
-        prevpt = new go.Point(Math.sqrt(thisloc.distanceSquaredPoint(prevloc)) / 4, 0);
-        prevpt.rotate(avg + (clockwise ? 90 : -90));
-        prevpt.add(thisloc);
-        nextpt = new go.Point(Math.sqrt(thisloc.distanceSquaredPoint(nextloc)) / 4, 0);
-        nextpt.rotate(avg - (clockwise ? 90 : -90));
-        nextpt.add(thisloc);
-      } else if (nextloc !== null) {
-        prevpt = null;
-        nextpt = thisloc;  // fix this point after the loop
-      } else if (prevloc !== null) {
-        var lastpt = this.getPoint(this.pointsCount - 1);
-        prevpt = thisloc;  // fix this point after the loop
-        nextpt = null;
-      }
-    }
-    if (prevpt !== null) this.addPoint(prevpt);
-    this.addPoint(thisloc);
-    if (nextpt !== null) this.addPoint(nextpt);
-    prevloc = thisloc;
-  }
-  // fix up the end points when it's Bezier
-  if (this.curve === go.Link.Bezier) {
-    // fix up the first point and the first control point
-    var start = this.getLinkPointFromPoint(this.fromNode, this.fromPort, this.fromPort.getDocumentPoint(go.Spot.Center), this.getPoint(3), true);
-    var ctrl2 = this.getPoint(2);
-    this.setPoint(0, start);
-    this.setPoint(1, new go.Point((start.x * 3 + ctrl2.x) / 4, (start.y * 3 + ctrl2.y) / 4));
-    // fix up the last point and the last control point
-    var end = this.getLinkPointFromPoint(this.toNode, this.toPort, this.toPort.getDocumentPoint(go.Spot.Center), this.getPoint(this.pointsCount - 4), false);
-    var ctrl1 = this.getPoint(this.pointsCount - 3);
-    this.setPoint(this.pointsCount - 2, new go.Point((end.x * 3 + ctrl1.x) / 4, (end.y * 3 + ctrl1.y) / 4));
-    this.setPoint(this.pointsCount - 1, end);
-  }
-  return true;
-};
-*/
-/*
-    { key: 1, text: "Alpha", color: "lightblue" },
-    { key: 2, text: "Beta", color: "orange" },
-    { key: 3, text: "Gamma", color: "lightgreen", group: 5 },
-    { key: 4, text: "Delta", color: "pink", group: 5 },
-    { key: 5, text: "Epsilon", color: "green", isGroup: true }
-
-    { from: 1, to: 2, color: "blue" },
-    { from: 2, to: 2 },
-    { from: 3, to: 4, color: "green" },
-    { from: 3, to: 1, color: "purple" }
-
-*/
